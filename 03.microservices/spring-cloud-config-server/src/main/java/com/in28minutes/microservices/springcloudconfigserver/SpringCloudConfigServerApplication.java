b9ecf0d7302a4f7d9e97b4d302e2b3a421a3f82d
VaibhavPawar123 <vaibhav92jun@gmail.com>
2019-11-18 08:16:40 +0530

    multipe .properties files

---
+++ 03.microservices/spring-cloud-config-server/src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.in28minutes.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer  //this annotation is necessary because if not given if hit the default page of config server with port 8888 i,e http://localhost:8888/limits-service/default it will give you error  page
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+}
